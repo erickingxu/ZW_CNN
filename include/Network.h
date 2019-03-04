0ddee5d9d19ab6d2cc5d7f5ebd0b836cfdd7a477
BBuf <1182563586@qq.com>
2019-03-04 15:33:24 +0800

    Update zxy_neural_network Network.cpp

--- include/Network.h
+++ include/Network.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <ctime>
+#include <random>
 #include <opencv2\opencv.hpp>
 using namespace std;
 using namespace cv;
@@ -15,9 +17,19 @@ public:
 	vector <Mat> weights;
 	//偏置矩阵
 	vector <Mat> biases;
+	//初始化策略
+	//0表示[-1，1]随机初始化
+	//1表示均值为GaussMean方差为GaussSigma的高斯初始化
+	int InitType = 0;
+	float GaussSigma = 1.0;
+	float GaussMean = 0;
 public:
 	Net() {};
 	~Net() {};
 	void initNet(vector <int> layer_neuron_num_);
+	float *random_uniform(int length);
+	float *random_gaussian(int length);
+	void initWeights();
+	void initBiases();
 
 };
--- src/Network.cpp
+++ src/Network.cpp
@@ -1,4 +1,4 @@
-#include "Network.h"
+#include "../include/Network.h"
 
 void Net::initNet(vector <int> layer_neuron_num_) {
 	//赋值
@@ -19,6 +19,70 @@ void Net::initNet(vector <int> layer_neuron_num_) {
 		weights.emplace_back(temp_weight);
 		biases.emplace_back(temp_biases);
 	}
-	cout << "============= Initialize zxy_neural_network Done! ====================" << endl;
+	cout << "============= Initialize zxy_neural_network Net Done! ====================" << endl;
 }
 
+float *Net::random_uniform(int length) {
+	float *dest = new float[length];
+	uniform_real_distribution<double> dist(-1.0, 1.0);
+	mt19937 rng;
+	rng.seed(random_device{}());
+	for (int i = 0; i < length; i++) {
+		dest[i] = dist(rng);
+	}
+	return dest;
+}
+
+inline float uniform_rand(float l, float r) {
+	return l + ((double)std::rand() / (RAND_MAX + 1.0)) * (r - l);
+}
+
+float *Net::random_gaussian(int length) {
+	float *dest = new float[length];
+	for (int i = 0; i < length; i++) {
+		double x, y, r2;
+		do {
+			x = -1.0 + 2.0 * uniform_rand(0.0, 1.0);
+			y = -1.0 + 2.0 * uniform_rand(0.0, 1.0);
+			r2 = x * x + y * y;
+		} while (r2 > 1.0 || r2 == 0.0);
+		dest[i] = Net::GaussMean + Net::GaussSigma * y * std::sqrt(-2.0 * log(r2) / r2);
+	}
+	return dest;
+}
+
+void Net::initWeights() {
+	int Size = weights.size();
+	for (int i = 0; i < Size; i++) {
+		int row = weights[i].rows;
+		int col = weights[i].cols;
+		float *data = new float[row * col];
+		if (Net::InitType == 0) {
+			data = Net::random_uniform(row * col);
+		}
+		else if (Net::InitType == 1) {
+			data = Net::random_gaussian(row * col);
+		}
+		Mat temp(row, col, CV_32FC1, data);
+		weights[i] = temp.clone();
+	}
+	cout << "============= Initialize zxy_neural_network Weights Done! ====================" << endl;
+}
+
+void Net::initBiases() {
+	int Size = biases.size();
+	for (int i = 0; i < Size; i++) {
+		int row = biases[i].rows;
+		int col = biases[i].cols;
+		float *data = new float[row * col];
+		if (Net::InitType == 0) {
+			data = Net::random_uniform(row * col);
+		}
+		else if (Net::InitType == 1) {
+			data = Net::random_gaussian(row * col);
+		}
+		Mat temp(row, col, CV_32FC1, data);
+		biases[i] = temp.clone();
+	}
+	cout << "============= Initialize zxy_neural_network Biases Done! ====================" << endl;
+}
