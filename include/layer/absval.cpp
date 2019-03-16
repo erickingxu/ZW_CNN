b84fd8ce3a8cc2a145578beaf1330385303aada3
zxyyyyyyyyyyyyyyy <35585791+BBuf@users.noreply.github.com>
2019-03-16 11:33:08 +0800

    Add AbsVal Layer

--- include/layer/absval.cpp
+++ include/layer/absval.cpp
@@ -1,19 +1,13 @@
-#include "absval.h"
+#pragma once
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
 
-vector <Mat> AbsVal::Activation() {
-	int Size = input.size();
-	int row = input[0].rows;
-	int col = input[1].cols;
-	vector <Mat> output;
-	output.resize(Size);
-	for (int i = 0; i < Size; i++) {
-		Mat dest(row, col, CV_32FC1);
-		for (int j = 0; j < row; j++) {
-			for (int k = 0; k < col; k++) {
-				dest.at<float>(j, k) = input[i].at<float>(j, k) >= 0 ? input[i].at<float>(j, k) : -(input[i].at<float>(j, k));
-			}
-		}
-		output[i] = dest;
-	}
-	return output;
-}
+class AbsVal {
+private:
+	vector <Mat> input;
+public:
+	AbsVal(vector <Mat> input_) :input(input_) {}
+	vector <Mat> Activation();
+};
