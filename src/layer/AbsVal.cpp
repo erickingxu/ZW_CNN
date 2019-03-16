c274fe87c332ed9e89c5b9a4aeab906cb49de547
BBuf <1182563586@qq.com>
2019-03-16 11:28:38 +0800

    Add AbsVal Layer

--- src/layer/AbsVal.cpp
+++ src/layer/AbsVal.cpp
@@ -1,17 +1,19 @@
-#include "AbsVal.h"
-#include "config.h"
+#include "absval.h"
 
-Mat AbsVal::Activation(){
-	int row = input.rows;
-	int col = input.cols;
-	Mat dest(row, col, CV_32FC1);
-
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			dest.at<float>(i, j) = input.at<float>(i, j) >= 0 ? input.at<float>(i, j) :  - (input.at<float>(i, j));
+vector <Mat> AbsVal::Activation() {
+	int Size = input.size();
+	int row = input[0].rows;
+	int col = input[1].cols;
+	vector <Mat> output;
+	output.resize(Size);
+	for (int i = 0; i < Size; i++) {
+		Mat dest(row, col, CV_32FC1);
+		for (int j = 0; j < row; j++) {
+			for (int k = 0; k < col; k++) {
+				dest.at<float>(j, k) = input[i].at<float>(j, k) >= 0 ? input[i].at<float>(j, k) : -(input[i].at<float>(j, k));
+			}
 		}
+		output[i] = dest;
 	}
-
-	return dest;
-
+	return output;
 }
