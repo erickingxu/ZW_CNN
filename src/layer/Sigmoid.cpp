a2e875dd1c23a784a4162a231f9383bfe0746501
BBuf <1182563586@qq.com>
2019-03-16 11:03:11 +0800

    Add Sigmoid Layer

--- src/layer/Sigmoid.cpp
+++ src/layer/Sigmoid.cpp
@@ -1,4 +1,4 @@
-#include "include/layer/Sigmoid.h"
+#include "Sigmoid.h"
 #include "config.h"
 
 //e^x
@@ -10,48 +10,26 @@
 //	return x;
 //}
 
-Mat Sigmoid::Activation() {
-	int row = input.rows;
-	int col = input.cols;
-	Mat dest(row, col, CV_32FC1);
-	Mat exp_x(row, col, CV_32FC1);
-	//#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			exp_x.at<float>(i, j) = exp(-input.at<float>(i, j));
+vector <Mat> Sigmoid::Activation() {
+	int Size = input.size();
+	int row = input[0].rows;
+	int col = input[0].cols;
+	vector <Mat> output;
+	output.resize(Size);
+	for (int i = 0; i < Size; i++) {
+		Mat dest(row, col, CV_32FC1);
+		Mat exp_x(row, col, CV_32FC1);
+		for (int j = 0; j < row; j++) {
+			for (int k = 0; k < col; k++) {
+				exp_x.at<float>(j, k) = exp(-input[i].at<float>(j, k));
+			}
 		}
-	}
-
-	//#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			dest.at<float>(i, j) = 1.0 / (1.0 + exp_x.at<float>(i, j));
+		for (int j = 0; j < row; j++) {
+			for (int k = 0; k < col; k++) {
+				dest.at<float>(j, k) = 1.0 / (1.0 + exp_x.at<float>(j, k));
+			}
 		}
+		output[i] = dest;
 	}
-
-	return dest;
-}
-
-Mat Sigmoid::DeActivation() {
-	int row = input.rows;
-	int col = input.cols;
-	Mat psigmoidx = Sigmoid::Activation();
-	Mat nsigmoidx(row, col, CV_32FC1);
-	Mat dest(row, col, CV_32FC1);
-
-	//#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			nsigmoidx.at<float>(i, j) = 1.0 - psigmoidx.at<float>(i, j);
-		}
-	}
-
-	//#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			dest.at<float>(i, j) = psigmoidx.at<float>(i, j) * nsigmoidx.at<float>(i, j);
-		}
-	}
-
-	return dest;
+	return output;
 }
