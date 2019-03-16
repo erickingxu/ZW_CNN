d1162b464921e82d042d99cda0b862d7fb285e21
BBuf <1182563586@qq.com>
2019-03-16 11:12:21 +0800

    Add Tanh Layer

--- src/layer/Tanh.cpp
+++ src/layer/Tanh.cpp
@@ -1,4 +1,4 @@
-#include "include/layer/Tanh.h"
+#include "Tanh.h"
 #include "config.h"
 
 //e^x
@@ -10,56 +10,33 @@
 //	return x;
 //}
 
-Mat Tanh::Activation() {
-	int row = input.rows;
-	int col = input.cols;
-	Mat pos_exp_x(row, col, CV_32FC1);
-	Mat neg_exp_x(row, col, CV_32FC1);
-	Mat dest(row, col, CV_32FC1);
-#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			pos_exp_x.at<float>(i, j) = exp(input.at<float>(i, j));
+vector <Mat> Tanh::Activation() {
+	int Size = input.size();
+	int row = input[0].rows;
+	int col = input[0].cols;
+	vector <Mat> output;
+	output.resize(Size);
+
+	for (int i = 0; i < Size; i++) {
+		Mat pos_exp_x(row, col, CV_32FC1);
+		Mat neg_exp_x(row, col, CV_32FC1);
+		Mat dest(row, col, CV_32FC1);
+		for (int j = 0; j < row; j++) {
+			for (int k = 0; k < col; k++) {
+				pos_exp_x.at<float>(j, k) = exp(input[i].at<float>(j, k));
+			}
 		}
-	}
-
-#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			neg_exp_x.at<float>(i, j) = exp(-input.at<float>(i, j));
+		for (int j = 0; j < row; j++) {
+			for (int k = 0; k < col; k++) {
+				neg_exp_x.at<float>(j, k) = exp(-input[i].at<float>(j, k));
+			}
 		}
-	}
-
-#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			dest.at<float>(i, j) = (pos_exp_x.at<float>(i, j) - neg_exp_x.at<float>(i, j)) / (pos_exp_x.at<float>(i, j) + neg_exp_x.at<float>(i, j));
+		for (int j = 0; j < row; j++) {
+			for (int k = 0; k < col; k++) {
+				dest.at<float>(j, k) = (pos_exp_x.at<float>(j, k) - neg_exp_x.at<float>(j, k)) / (pos_exp_x.at<float>(j, k) + neg_exp_x.at<float>(j, k));
+			}
 		}
+		output.push_back(dest);
 	}
-
-	return dest;
-}
-
-Mat Tanh::DeActivation() {
-	int row = input.rows;
-	int col = input.cols;
-	Mat tanhx = Tanh::Activation();
-	Mat tanhx2(row, col, CV_32FC1);
-	Mat dest(row, col, CV_32FC1);
-
-#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			tanhx2.at<float>(i, j) = tanhx.at<float>(i, j) * tanhx.at<float>(i, j);
-		}
-	}
-
-#pragma omp parallel for num_threads(openmp_num_threads)
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			dest.at<float>(i, j) = 1.0 - tanhx2.at<float>(i, j);
-		}
-	}
-
-	return dest;
+	return output;
 }
