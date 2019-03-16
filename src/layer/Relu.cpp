2007b39861cbc45454013fb22a2061b85eae75f6
BBuf <1182563586@qq.com>
2019-03-16 10:56:11 +0800

    Add ReLU Layer

--- src/layer/Relu.cpp
+++ src/layer/Relu.cpp
@@ -1,21 +1,17 @@
 #include "Relu.h"
 #include "config.h"
 
-vector <Mat> Relu::forward() {
+vector <Mat> Relu::Activation() {
 	vector <Mat> output;
-	output.resize(input.size());
-	for (int i = 0; i < input.size(); i++) {
-		int row = input[i].rows;
-		int col = input[i].cols;
-		Mat dest(row, col, CV_32FC1);
-#pragma omp parallel for num_threads(openmp_num_threads)
-		for (int j = 0; j < row; j++) {
-			for (int k = 0; k < col; k++) {
-				dest.at<float>(j, k) = input[i].at<float>(j, k) > 0 ? input[i].at<float>(j, k) : 0;
+	int Size = input.size();
+	for (int i = 0; i < Size; i++) {
+		Mat temp(input[i].rows, input[i].cols, CV_32FC1);
+		for (int j = 0; j < input[i].rows; j++) {
+			for (int k = 0; k < input[i].cols; k++) {
+				temp.at<float>(i, j) = input[i].at<float>(j, k) > 0 ? input[i].at<float>(j, k) : 0;
 			}
 		}
-		output[i] = dest;
+		output.push_back(temp);
 	}
 	return output;
 }
-
