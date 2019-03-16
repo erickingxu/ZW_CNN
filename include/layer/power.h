f09d60b1c2d6061c48ae2accea12e18abf97c02f
zxyyyyyyyyyyyyyyy <35585791+BBuf@users.noreply.github.com>
2019-03-16 11:49:26 +0800

    Add Power Layer

--- include/layer/power.h
+++ include/layer/power.h
@@ -10,6 +10,7 @@ private:
 	float shift;
 	float scale;
 	float power;
+	
 public:
 	Power(vector <Mat> input_, float shift_ = 0, float scale_ = 1.0, float power_ = 1.0) : 
 		input(input_), shift(shift_), scale(scale_), power(power_) {}
