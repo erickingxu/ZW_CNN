3b684623bf2505c278c24e6915e9e54a252e54b2
zxyyyyyyyyyyyyyyy <35585791+BBuf@users.noreply.github.com>
2019-03-16 11:54:54 +0800

    Add BNLL Layer

---
+++ src/layer/bnll.cpp
@@ -0,0 +1,25 @@
+#include "bnll.h"
+
+vector <Mat> BNLL::Activation() {
+	int Size = input.size();
+	int row = input[0].rows;
+	int col = input[1].cols;
+	vector <Mat> output;
+	output.resize(Size);
+	for (int i = 0; i < Size; i++) {
+		Mat dest(row, col, CV_32FC1);
+		Mat exp_x(row, col, CV_32FC1);
+		for (int j = 0; j < row; j++) {
+			for (int k = 0; k < col; k++) {
+				exp_x.at<float>(j, k) = exp(input[i].at<float>(j, k));
+			}
+		}
+		for (int j = 0; j < row; j++) {
+			for (int k = 0; k < col; k++) {
+				dest.at<float>(j, k) = log(1.0 + exp_x[i].at<float>(j, k));
+			}
+		}
+		output[i] = dest;
+	}
+	return output;
+}
