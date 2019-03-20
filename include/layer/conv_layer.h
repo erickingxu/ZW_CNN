19da8051b8dddfcea2a2b2348f903fef657d8ea6
BBuf <1182563586@qq.com>
2019-03-20 17:48:35 +0800

    Add Convolution Layer

--- include/layer/conv_layer.h
+++ include/layer/conv_layer.h
@@ -17,12 +17,12 @@ private:
 	int kernel_width;
 	int kernel_stride_height = 1;
 	int kernel_stride_width = 1;
-	bool padding = true;
+	string padding = "VALID";
 	vector <Mat>input;
 	vector <Mat>kernel;
 public:
 	Convolution(vector <Mat>input_, vector <Mat>kernel_, int kernel_height_, int kernel_width_, int kernel_stride_height_, int kernel_stride_width_,
-		bool padding_) :input(input_), kernel(kernel_), kernel_height(kernel_height_), kernel_width(kernel_width),
+		string padding_) :input(input_), kernel(kernel_), kernel_height(kernel_height_), kernel_width(kernel_width),
 		kernel_stride_height(kernel_stride_height_), kernel_stride_width(kernel_stride_width_), padding(padding_) {}
 	Convolution(vector <Mat>input_, vector <Mat>kernel_, int kernel_height_, int kernel_width_) :input(input_), kernel(kernel_), kernel_height(kernel_height_), kernel_width(kernel_width) {}
 	vector<Mat> Activation();
