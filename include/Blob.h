0ea64a6e41e8b60d906dad29aedb3efc5bcdb07f
BBuf <1182563586@qq.com>
2019-03-23 22:53:25 +0800

    Add Blob.h

---
+++ include/Blob.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+using namespace std;
+using namespace cv;
+
+class Blob {
+public:
+	Blob(){}
+	string name;
+	//生产这个blob的层的index
+	int producer;
+	//需要这个blob作为输入的层index
+	vector <int> consumers;
+};
