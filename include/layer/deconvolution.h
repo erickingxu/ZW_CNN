ef58631fef57ed7c07c1bd24d86197fc9ebe97a5
BBuf <1182563586@qq.com>
2019-03-23 17:24:16 +0800

    Add Deconvolution Layer

--- include/layer/deconvolution.h
+++ include/layer/deconvolution.h
@@ -8,12 +8,12 @@ class Deconvolution {
 private:
 	int kernel_height;
 	int kernel_width;
-	int kernel_height_stride = 1;
-	int kernel_width_stride = 1;
+	int stride;
+	string padding = "VALID";
 	vector <Mat> input;
 	vector <Mat> kernel;
 public:
-	Deconvolution(int kernel_height_, int kernel_width_, int kernel_height_stride_, int kernel_width_stride_, vector <Mat> input_, vector <Mat> kernel_):
-		kernel_height(kernel_height_), kernel_width(kernel_width_), kernel_height_stride(kernel_height_stride_), kernel_width_stride(kernel_width_stride_), input(input_), kernel(kernel_){}
+	Deconvolution(int kernel_height_, int kernel_width_, int stride_, string padding_, vector <Mat> input_, vector <Mat> kernel_) :
+		kernel_height(kernel_height_), kernel_width(kernel_width_), stride(stride_), padding(padding_), input(input_), kernel(kernel_) {}
 	vector <Mat> Activation();
 };
--- src/layer/deconvolution.cpp
+++ src/layer/deconvolution.cpp
@@ -1,4 +1,7 @@
 #include "deconvolution.h"
+#include "fractionally_padding.h"
+#include "im2col_stride_1x1.h"
+#include "im2col_stride_nxn.h"
 
 vector <Mat> Deconvolution::Activation() {
 	int input_length = input.size();
@@ -6,10 +9,28 @@ vector <Mat> Deconvolution::Activation() {
 	vector <Mat> output;
 	output.resize(output_length);
 	for (int i = 0; i < output_length; i += input_length) {
+		vector <Mat> temp;
+		temp.resize(input_length);
+		for (int j = i; j < i + input_length; j++) {
+			temp[j] = input[j];
+		}
+		Fraction frac(temp, stride, padding, kernel_height, kernel_width);
+		vector <Mat> out = frac.Activation();
 		if (input_length == 1) {
-			int output_height = (input[i].rows - 1) * kernel_height_stride + kernel_height;
-			int output_width = (input[i].cols - 1) * kernel_width_stride + kernel_width;
-
+			IM2COL_1x1 ZW(out[0], kernel[i], stride, stride, padding);
+			output[i] = ZW.ZW_IM2COL();
+		}
+		else {
+			vector <Mat> out;
+			for (int j = 0; j < input_length; j++) {
+				IM2COL_1x1 ZW(input[j], kernel[i + j], stride, stride, padding);
+				out.push_back(ZW.ZW_IM2COL());
+			}
+			for (int j = 1; j < input_length; j++) {
+				out[0] = out[0] + out[j];
+			}
+			output[i] = out[0];
 		}
 	}
+	return output;
 }
