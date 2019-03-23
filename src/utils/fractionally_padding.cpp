b51eae180ac73afd01dd54e4506dc81df085998a
BBuf <1182563586@qq.com>
2019-03-23 15:08:41 +0800

    Add Fractionally Padding

--- src/utils/fractionally_padding.cpp
+++ src/utils/fractionally_padding.cpp
@@ -15,25 +15,37 @@ vector <Mat> Fraction::Activation() {
 			int newrow = row + (kernel_height - 1) + (row - 1) * (stride - 1);
 			int newcol = col + (kernel_width - 1) + (col - 1) * (stride - 1);
 			Mat dest(newrow, newcol, CV_32FC1);
-			for (int i = 0; i < newrow; i++) {
-				for (int j = 0; j < newcol; j++) {
-					if (i < (kernel_height - 1) / 2 || (newrow - i) <= (kernel_height - 1) / 2 || j < (kernel_width - 1) / 2 || (newcol - j) <= (kernel_width - 1) / 2) {
-						dest.at<float>(i, j) = 0;
+			for (int j = 0; j < newrow; j++) {
+				for (int k = 0; k < newcol; k++) {
+					if (j < (kernel_height - 1) / 2 || (newrow - j) <= (kernel_height - 1) / 2 || k < (kernel_width - 1) / 2 || (newcol - k) <= (kernel_width - 1) / 2) {
+						dest.at<float>(j, k) = 0;
 					}
-					else if ((i - (kernel_height - 1) / 2) % stride == 0 && (j - (kernel_width - 1) / 2) % stride == 0) {
-						dest.at<float>(i, j) = input[i].at<float>((i - (kernel_height - 1) / 2) / stride, (j - (kernel_width - 1) / 2) / stride);
+					else if ((j - (kernel_height - 1) / 2) % stride == 0 && (k - (kernel_width - 1) / 2) % stride == 0) {
+						dest.at<float>(j, k) = input[i].at<float>((j - (kernel_height - 1) / 2) / stride, (k - (kernel_width - 1) / 2) / stride);
 					}
 					else {
-						dest.at<float>(i, j) = 0;
+						dest.at<float>(j, k) = 0;
 					}
 				}
 			}
+			output[i] = dest;
 		}
 		else {
 			int newrow = row + (row - 1) * (stride - 1);
 			int newcol = col + (col - 1) * (stride - 1);
 			Mat dest(newrow, newcol, CV_32FC1);
-
+			for (int j = 0; j < newrow; j++) {
+				for (int k = 0; k < newcol; k++) {
+					if (j % stride == 0 && k % stride == 0) {
+						dest.at<float>(j, k) = input[i].at<float>(j / stride, k / stride);
+					}
+					else {
+						dest.at<float>(j, k) = 0;
+					}
+				}
+			}
+			output[i] = dest;
 		}
 	}
+	return output;
 }
